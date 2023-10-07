2161c8c33924d43a0f129c9338b69c1c7d46aee1
Web Masta <webmastaman@yahoo.com>
2023-10-06 23:22:44 -0400

    Fixed game from freezing on load.

--- Settings/Settings.cs
+++ Settings/Settings.cs
@@ -67,9 +67,7 @@ namespace WOTR_MAKING_FRIENDS.Settings
             // Load Localizations
             LocalizationTool.LoadEmbeddedLocalizationPacks(
               "WOTR_MAKING_FRIENDS.Output.Localizations.Settings.json",
-              "WOTR_MAKING_FRIENDS.Output.Localizations.SummonerClass.json",
               "WOTR_MAKING_FRIENDS.Output.Localizations.CharacterClasses.json",
-              "WOTR_MAKING_FRIENDS.Output.Localizations.SummonerSpellbook.json",
               "WOTR_MAKING_FRIENDS.Output.Localizations.Spells.json",
               "WOTR_MAKING_FRIENDS.Output.Localizations.Scrolls.json",
               "WOTR_MAKING_FRIENDS.Output.Localizations.Units.json",
